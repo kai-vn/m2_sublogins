d8390d78d527face9aa4802c511d8b47803bb18d
DESKTOP-UV53FSV\CLAY <longnv@azebiz.com>
2017-07-27 14:06:07 +0700

    fix feedback #9 and #7
    9. Sub-account should not be able to see "My Sub-account Orders" & cannot create Sub-account
    7. When creating sub-account in frontend, the left menu is not correct as well:

---
+++ Block/SubAc.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace SITC\Sublogins\Block;
+
+class SubAc extends \Magento\Framework\View\Element\Template
+{
+
+    protected $helper;
+    public function __construct(
+        \SITC\Sublogins\Helper\Data $helper,
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        $this->helper = $helper;
+        parent::__construct($context, $data);
+    }
+    protected function _prepareLayout()
+    {
+        if($this->helper->isSublogin()) {
+            $this->addChild(
+                'list-sub-account',
+                'Magento\Framework\View\Element\Html\Link\Current',
+                [
+                    'label' => 'List Sub Account',
+                    'path' => 'sublogins/account/listsubaccount'
+                ]
+            );
+
+        }elseif($this->helper->getIdSub()) {
+            $this->addChild(
+                'list-sub-account',
+                'Magento\Framework\View\Element\Html\Link\Current',
+                [
+                    'label' => 'List Sub Account',
+                    'path' => 'sublogins/account/listsubaccount'
+                ]
+            );
+            $this->addChild(
+                'create-sub-account',
+                'Magento\Framework\View\Element\Html\Link\Current',
+                [
+                    'label' => 'Create Sub Account',
+                    'path' => 'sublogins/create/account'
+                ]
+            );
+            $this->addChild(
+                'order-sub-account',
+                'Magento\Framework\View\Element\Html\Link\Current',
+                [
+                    'label' => 'My Sub Account Order',
+                    'path' => 'sublogins/order/history'
+                ]
+            );
+        }
+        return parent::_prepareLayout();
+    }
+}
