5f73dd4feaeda6016b6ca6355c748417b0dc4769
LongNgo <longnv@azebiz.com>
2017-08-11 10:14:07 +0700

    fix redirect

--- Controller/Adminhtml/Account/Save.php
+++ Controller/Adminhtml/Account/Save.php
@@ -19,27 +19,125 @@ use Magento\Customer\Model\Metadata\Form;
  */
 class Save extends \Magento\Customer\Controller\Adminhtml\Index\Save
 {
+    private $emailNotification;
     public function execute()
     {
-        $response = parent::execute();
         $returnToEdit = false;
+        $originalRequestData = $this->getRequest()->getPostValue();
         $customerId = $this->getCurrentCustomerId();
+        if ($originalRequestData) {
+            try {
+                // optional fields might be set in request for future processing by observers in other modules
+                $customerData = $this->_extractCustomerData();
+                $addressesData = $this->_extractCustomerAddressData($customerData);
+                if ($customerId) {
+                    $currentCustomer = $this->_customerRepository->getById($customerId);
+                    $customerData = array_merge(
+                        $this->customerMapper->toFlatArray($currentCustomer),
+                        $customerData
+                    );
+                    $customerData['id'] = $customerId;
+                }
+
+                /** @var CustomerInterface $customer */
+                $customer = $this->customerDataFactory->create();
+                $this->dataObjectHelper->populateWithArray(
+                    $customer,
+                    $customerData,
+                    '\Magento\Customer\Api\Data\CustomerInterface'
+                );
+                $addresses = [];
+                foreach ($addressesData as $addressData) {
+                    $region = isset($addressData['region']) ? $addressData['region'] : null;
+                    $regionId = isset($addressData['region_id']) ? $addressData['region_id'] : null;
+                    $addressData['region'] = [
+                        'region' => $region,
+                        'region_id' => $regionId,
+                    ];
+                    $addressDataObject = $this->addressDataFactory->create();
+                    $this->dataObjectHelper->populateWithArray(
+                        $addressDataObject,
+                        $addressData,
+                        '\Magento\Customer\Api\Data\AddressInterface'
+                    );
+                    $addresses[] = $addressDataObject;
+                }
+
+                $this->_eventManager->dispatch(
+                    'adminhtml_customer_prepare_save',
+                    ['customer' => $customer, 'request' => $this->getRequest()]
+                );
+                $customer->setAddresses($addresses);
+                if (isset($customerData['sendemail_store_id'])) {
+                    $customer->setStoreId($customerData['sendemail_store_id']);
+                }
+
+                // Save customer
+                if ($customerId) {
+                    $this->_customerRepository->save($customer);
+                    $this->getEmailNotification()->credentialsChanged($customer, $currentCustomer->getEmail());
+                } else {
+                    $customer = $this->customerAccountManagement->createAccount($customer);
+                    $customerId = $customer->getId();
+                }
+
+                $isSubscribed = null;
+                if ($this->_authorization->isAllowed(null)) {
+                    $isSubscribed = $this->getRequest()->getPost('subscription');
+                }
+                if ($isSubscribed !== null) {
+                    if ($isSubscribed !== 'false') {
+                        $this->_subscriberFactory->create()->subscribeCustomerById($customerId);
+                    } else {
+                        $this->_subscriberFactory->create()->unsubscribeCustomerById($customerId);
+                    }
+                }
+
+                // After save
+                $this->_eventManager->dispatch(
+                    'adminhtml_customer_save_after',
+                    ['customer' => $customer, 'request' => $this->getRequest()]
+                );
+                $this->_getSession()->unsCustomerFormData();
+                // Done Saving customer, finish save action
+                $this->_coreRegistry->register(RegistryConstants::CURRENT_CUSTOMER_ID, $customerId);
+                $this->messageManager->addSuccess(__('You saved the customer.'));
+                $returnToEdit = (bool)$this->getRequest()->getParam('back', false);
+            } catch (\Magento\Framework\Validator\Exception $exception) {
+                $messages = $exception->getMessages();
+                if (empty($messages)) {
+                    $messages = $exception->getMessage();
+                }
+                $this->_addSessionErrorMessages($messages);
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            } catch (LocalizedException $exception) {
+                $this->_addSessionErrorMessages($exception->getMessage());
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            } catch (\Exception $exception) {
+                $this->messageManager->addException($exception, __('Something went wrong while saving the customer.'));
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            }
+        }
+        $resultRedirect = $this->resultRedirectFactory->create();
         if ($returnToEdit) {
             if ($customerId) {
-                $response->setPath(
+                $resultRedirect->setPath(
                     'sublogins/account/edit',
                     ['id' => $customerId, '_current' => true]
                 );
             } else {
-                $response->setPath(
+                $resultRedirect->setPath(
                     'sublogins/account/new',
                     ['_current' => true]
                 );
             }
         } else {
-            $response->setPath('sublogins/account/index');
+            $resultRedirect->setPath('sublogins/account/index');
         }
-        return $response;
+        return $resultRedirect;
     }
     private function getCurrentCustomerId()
     {
@@ -51,4 +149,16 @@ class Save extends \Magento\Customer\Controller\Adminhtml\Index\Save
 
         return $customerId;
     }
+    private function getEmailNotification()
+    {
+        if (!($this->emailNotification instanceof EmailNotificationInterface)) {
+            return \Magento\Framework\App\ObjectManager::getInstance()->get(
+                EmailNotificationInterface::class
+            );
+        } else {
+            return $this->emailNotification;
+        }
+    }
+
+
 }
